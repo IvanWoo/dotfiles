96d16db24ed107dad702334f3e8fca007e33fecd
Yifan Wu <tyivanwu@gmail.com>
2023-01-30 22:51:27 -0500

    fix: revert the done changes

--- fish/conf.d/done.fish
+++ fish/conf.d/done.fish
@@ -5,11 +5,16 @@
 #
 # It is invoked by the fish shell automatically using its event system.
 function __postexec_notify_on_long_running_commands --on-event fish_postexec
-    if status is-interactive
+    set --function interactive_commands 'vim' 'man' 'less' 'bash' 'zsh' 'fish' 'htop' 'k9s'
+    set --function command (string split ' ' $argv[1])
+    if contains $command $interactive_commands
+        # We quit interactive commands manually,
+        # no need for a notification.
         return
     end
 
-    if test $CMD_DURATION -gt 5000
-        terminal-notifier -title 'command finished' -message "$argv" -sound default
+    if test $CMD_DURATION -gt 10000
+        set --local seconds (math $CMD_DURATION/1000)
+        terminal-notifier -title "command finished in $seconds s" -message "$argv" -sound default
     end
 end
