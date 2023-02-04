785bc1ee3380ddf0c8dae5cc379f54d6b465cefe
Yifan Wu <tyivanwu@gmail.com>
2023-02-04 14:08:13 -0500

    chore(fish): abstract the alias

---
+++ fish/conf.d/alias.fish
@@ -0,0 +1,22 @@
+# setup the default alias
+# how to: add below code snippit into the config.fish
+# if type -q __default_alias
+#     __default_alias
+# end
+function __default_alias
+    alias vim nvim
+    alias docker nerdctl
+
+    alias k "kubectl"
+    alias kctx "kubectl config use-context (kubectl config get-contexts -o=name | fzf)"
+
+    # git
+    alias glo "git log --oneline"
+
+    # kitty integration
+    if set -q KITTY_INSTALLATION_DIR
+        alias icat="kitty +kitten icat --align=left"
+        alias imgcat=icat
+        alias d="kitty +kitten diff"
+    end
+end
